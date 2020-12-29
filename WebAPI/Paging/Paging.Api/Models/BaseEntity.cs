ae89ace12396853f5c9b88b8f5f1458e1ae2ac6f
Ahanur Ahundov <ahanur.ahundov@synapse.com>
2020-12-29 16:41:21 +0200

    Add base entities

---
+++ WebAPI/Paging/Paging.Api/Models/BaseEntity.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Paging.Api.Models
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ WebAPI/Paging/Paging.Api/Models/Book.cs
@@ -0,0 +1,10 @@
+namespace Paging.Api.Models
+{
+    public class Book : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public int Capacity { get; set; }
+        public string Publisher { get; set; }
+    }
+}
