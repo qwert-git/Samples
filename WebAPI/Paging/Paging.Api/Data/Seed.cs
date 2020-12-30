ff22caf71fc0470e1538f2a396fe992c2cd709fa
Ahanur Ahundov <ahanur.ahundov@synapse.com>
2020-12-30 10:04:51 +0200

    Add seeding

---
+++ WebAPI/Paging/Paging.Api/Data/Seed.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using Paging.Api.Models;
+
+namespace Paging.Api.Data
+{
+    public static class Seed
+    {
+        public static void PopulateBooks(ApiContext context)
+        {
+            var books = new List<Book>
+            {
+                new Book { Id = Guid.NewGuid(), Name = "In Search of Lost Time", Author = "Marcel Proust", Capacity = 134, Publisher = "Book Publisher in UA" },
+                new Book { Id = Guid.NewGuid(), Name = "One Hundred Years of Solitude", Author = "Gabriel Garcia Marquez", Capacity = 221, Publisher = "Book Publisher in UA" },
+                new Book { Id = Guid.NewGuid(), Name = "The Great Gatsby", Author = "F. Scott Fitzgerald", Capacity = 174, Publisher = "Book Publisher in UA" },
+                new Book { Id = Guid.NewGuid(), Name = "Moby Dick", Author = "Herman Melville", Capacity = 244, Publisher = "Book Publisher in UA" }
+            };
+
+            context.Books.AddRange(books);
+            context.SaveChanges();
+        }
+    }
+}
--- WebAPI/Paging/Paging.Api/Program.cs
+++ WebAPI/Paging/Paging.Api/Program.cs
@@ -1,11 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Paging.Api.Data;
 
 namespace Paging.Api
 {
@@ -13,7 +9,14 @@ namespace Paging.Api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using var scope = host.Services.CreateScope();
+            using var context = scope.ServiceProvider.GetRequiredService<ApiContext>();
+
+            Seed.PopulateBooks(context);
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
