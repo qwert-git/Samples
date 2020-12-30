0bc19e3e7d3647c4928a32b974e5075fc6e31b90
Ahanur Ahundov <ahanur.ahundov@synapse.com>
2020-12-30 10:02:17 +0200

    Add EF Context

---
+++ WebAPI/Paging/Paging.Api/Data/ApiContext.cs
@@ -0,0 +1,25 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Paging.Api.Models;
+
+namespace Paging.Api.Data
+{
+    public class ApiContext : DbContext
+    {
+        private readonly IConfiguration _config;
+
+        public ApiContext(DbContextOptions<ApiContext> options, IConfiguration config) : base(options)
+        {
+            _config = config;
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (_config.GetValue<bool>("AppSettings:EnableDBTrace", false))
+                optionsBuilder.LogTo(Console.WriteLine);
+        }
+    }
+}
