34931d1b90ba361c5e73f9bbef06106414995a80
Ahanur Ahundov <ahanur.ahundov@synapse.com>
2021-01-03 15:34:15 +0200

    Add domain models

---
+++ WebAPI/Auth/Auth.API/Models/AppRole.cs
@@ -0,0 +1,7 @@
+namespace Auth.API.Models
+{
+    public class AppRole
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ WebAPI/Auth/Auth.API/Models/AppUser.cs
@@ -0,0 +1,8 @@
+namespace Auth.API.Models
+{
+    public class AppUser
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
