8251aaad30bb58d27a2c8472679c0c7e2fb275ab
Ahanur Ahundov <ahanur.ahundov@synapse.com>
2021-01-08 18:48:02 +0200

    Setup project

---
+++ WebAPI/HostedService/HostedService.API/Startup.cs
@@ -0,0 +1,65 @@
+using System;
+using HostedService.API.HostedServices;
+using HostedService.API.Services.ComicService;
+using HostedService.API.Services.HttpClientProxyService;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace HostedService.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "HostedService.API", Version = "v1" });
+            });
+
+            services.AddScoped<IHttpClientProxyService, HttpClientProxyService>();
+            services.AddScoped<IComicService, WebComicService>();
+
+            services.AddHttpClient("comicServer", options => options.BaseAddress = new Uri(Configuration["AppSettings:ComicBaseUrl"]));
+
+            services.AddHostedService<ComicCacheBackgroundService>();
+
+            services.AddMemoryCache();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "HostedService.API v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
